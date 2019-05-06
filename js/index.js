f89ceb29865a3c1bbfe9bdbb5b7248d7f22e08da
guoxh <guoxuehui@51jiabo.com>
2019-05-06 17:32:39 +0800

    plus

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 var vm = new Vue({
   el: '#app',
   data: {
+    lightVal: "1",
     todos: [
       { text: '学习 JavaScript' },
       { text: '学习 Vue' },
@@ -9,6 +10,9 @@ var vm = new Vue({
     tftext: "这是一段24px大小的字体"
   },
   created() {
+    //获取亮度0 - 1之间的数值
+    alert(plus.screen.getBrightness())
+    setTimeout(()=>{plus.screen.setBrightness( 0.5 );},20000)
     // this.$http.get("api").then(function (data) {
     //   console.log(data)
     // })
@@ -30,17 +34,22 @@ var vm = new Vue({
     //   // console.log(data)
     // })
 
-    axios({
-      url: "api",
-      method: "post",
-      data:  'mobile= xxxxxxxx',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }
-    }).then(function (data) {
-      console.log(data)
-    }).catch(function (err) {
-      console.log(err)
-    })
+    // axios({
+    //   url: "api",
+    //   method: "post",
+    //   data:  'mobile= xxxxxxxx',
+    //   headers: { 'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8' }
+    // }).then(function (data) {
+    //   console.log(data)
+    // }).catch(function (err) {
+    //   console.log(err)
+    // })
 
     // console.log(this.todos)
+  },
+  methods: {
+    setLight: function (val) {
+      plus.screen.setBrightness(val/100);
+    }
   }
 })
